dd97eb995a618e977a9af2faabb6108ed7ec71fd
Namra Patel <namrajpatel@gmail.com>
2021-07-05 16:57:09 -0600

    kovan setup not working, for test

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,12 +1,27 @@
 // Imports
 require("@nomiclabs/hardhat-waffle");
 
+
+// Go to https://www.alchemyapi.io, sign up, create
+// a new App in its dashboard, and replace "KEY" with its key
+const INFURA_API_KEY = "";
+
+// Replace this private key with your Ropsten account private key
+// To export your private key from Metamask, open Metamask and
+// go to Account Details > Export Private Key
+// Be aware of NEVER putting realnpx  Ether into testing accounts
+const KOVAN_PRIVATE_KEY = "";
+
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
   solidity: "0.8.0",
   networks: {
+    kovan: {
+      url: `https://kovan.infura.io/v3/${INFURA_API_KEY}`,
+      accounts: [`0x${KOVAN_PRIVATE_KEY}`]
+    },
     hardhat: {
       forking: {
         // Kovan RPC endpoint
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -7,11 +7,25 @@ async function deploy() {
       deployer.address
     );
   
-    const Waffle = await ethers.getContractFactory("Waffle");
-    const waffle = await Waffle.deploy();
+    const number = ethers.utils.parseEther("0.1");
+    
     const WaffleFactory = await ethers.getContractFactory("WaffleFactory");
-    const waffleFactory = await WaffleFactory.deploy();
-  
-    console.log("Waffle address: ", waffle.address);
+    const waffleFactory = await WaffleFactory.deploy
+    (
+      number, 
+      "0x6c3699283bda56ad74f6b855546325b68d482e983852a7a82979cc4807b641f4", 
+      "0xa36085F69e2889c224210F603D836748e7dC0088", 
+      "0xdD3782915140c8f3b190B5D67eAc6dc5760C46E9"
+      );
+    await waffleFactory.deployed();
+   
+
     console.log("WaffleFactory address: ", waffleFactory.address);
-  }
+    
+    main()
+      .then(() => process.exit(0))
+      .catch(error => {
+        console.error(error);
+        process.exit(1);
+      });
+    }
